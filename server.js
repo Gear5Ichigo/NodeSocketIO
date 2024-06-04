203f537e7fad212a980b03eb67beefa378c0592a
gear5Ichigo <ne.n.reh@baypath.net>
2024-06-04 14:03:10 -0400

    mongo

--- server.js
+++ server.js
@@ -2,19 +2,27 @@ const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io')
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const server = createServer(app);
+const mongo_client = new MongoClient("mongodb+srv://nenreh:mongoneh@schoolstuff.gjla1uc.mongodb.net/?retryWrites=true&w=majority&appName=SchoolStuff");
 const io = new Server(server);
 
+mongo_client.connect();
+
+const db = mongo_client.db("SocketIO");
+
 app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'))
+  res.sendFile(join(__dirname, 'site/index.html'))
 });
 
 io.on('connection', (socket) => {
+
   socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
+  
 });
 
 server.listen(3000, () => {
